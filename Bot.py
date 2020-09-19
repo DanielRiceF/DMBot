e2d2e2878ce7e891dacfdfb7289c2cf58d6310f7
DanielRice <dennekrov1@gmail.com>
2020-09-19 15:50:58 +0300

    Ver 1.0.3

--- Bot.py
+++ Bot.py
@@ -30,7 +30,7 @@ async def on_ready():
 	connection.commit()
 	print('Bot connected')
 @bot.event 
-async def on_member_join(memberm):
+async def on_member_join(member):
 	if cursor.execute(f"SELECT id FROM users WHERE id = {member.id}").fetchone() is  None:
 		cursor.execute(f"INSERT INTO users VALUES ('{member}',{member.id}, 0, 0 , 1 )")	
 		connection.commit()	
@@ -41,11 +41,11 @@ async def on_member_join(memberm):
 async def __balance(ctx,member:discord.Member = None):
 	if member is None:
 		await ctx.send(embed = discord.Embed(
-			description = f"""Уровень говнярства **{ctx.author}** . **{cursor.execute("SELECT cash FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]} мда, ну и вонь :coolstory:**"""
+			description = f"""Уровень говнярства **{ctx.author}** . **{cursor.execute("SELECT cash FROM users WHERE id = {}".format(ctx.author.id)).fetchone()[0]} мда, ну и вонь :coolstory**"""
 		))
 	else:
 		await ctx.send(embed = discord.Embed(
-			description = f"""Уровень говнярства **{member}** . **{cursor.execute("SELECT cash FROM users WHERE id = {}".format(ctx.member.id)).fetchone()[0]} мда, ну и вонь :poo: **"""
+			description = f"""Уровень говнярства **{member}** . **{cursor.execute("SELECT cash FROM users WHERE id = {}".format(ctx.member.id)).fetchone()[0]} мда, ну и вонь :poo **"""
 		))
 
 
